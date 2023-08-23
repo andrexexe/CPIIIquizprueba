7b73512765e5a5f88c4375c9f48ea8715b48d824
andrexexe <andresdmendez@gmail.com>
2023-08-23 13:27:52 -0500

    NUEVA RAMA

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,10 @@ fun main(args: Array<String>) {
 
     println("Hola ANdres")
 
+    println("Rama Numero2")
+
+    println(" Hola")
+
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
